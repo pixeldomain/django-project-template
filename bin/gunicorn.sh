6fe03e7760ea51b85e303a477028a671bb64e831
Darren O'Neill <darren@pixeldomain.co.uk>
2013-04-27 17:47:58 +0100

    Various changes. Plus add gunicorn_local.py.dist

---
+++ bin/gunicorn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+LOGFILE=/home/production/logs/logfile_gunicorn_{{ project_name }}
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=3
+USER=production
+GROUP=production
+ADDRESS=127.0.0.1:8001
+cd /home/production/public_html/{{ project_name }}/src/
+source /home/production/venv/{{ project_name }}/bin/activate
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn_django -w $NUM_WORKERS --bind=$ADDRESS \
+  --user=$USER --group=$GROUP --log-level=debug \
+  --log-file=$LOGFILE 2>>$LOGFILE
